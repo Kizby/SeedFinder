c5639c04268e7e9268dae56a17a96832379d5688
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-01-22 09:06:26 -0500

    Seems to work - clear away the debug info and make Seed a proper string

--- SeedFinder.cs
+++ SeedFinder.cs
@@ -8,90 +8,44 @@ namespace XRL.SeedFinder {
     using HarmonyLib;
     using XRL.Annals;
     using XRL.Core;
-    using XRL.Rules;
     using XRL.UI;
     using XRL.World;
-    using XRL.World.Parts;
 
-    [Serializable]
     public static class State {
         public const string Name = "Kizby"; // change this if you want
         public const int StartingLocation = 0; // {Joppa, marsh, dunes, canyon, hills}
 
-        public static ulong _Seed = 0;//Decode("6NQ");
-        public static string Seed {
-            get {
-                if (_Seed == 0 && File.Exists("seed.txt")) {
-                    using (StreamReader reader = new StreamReader("seed.txt")) {
-                        _ = ulong.TryParse(reader.ReadLine(), out _Seed);
-                    }
-                }
-                return Encode(_Seed);
-            }
-        }
+        public static string Seed;
+        public static int SeedLength = 8;
 
-        public static bool InGenerateEquipment = false;
-        public static int LastRoll = 999;
-        public static int BestRoll = 999;
-        public static bool WantNextRoll = false;
-
-        public static string LastReseed = "";
-        public static int RollsSinceReseed = 0;
-        public static string StateAtMeasure = "";
-
+        // set to true for significantly faster iteration of seeds, though the greater world won't be
+        // available for inspection and the game won't be in a playable state after
         public static bool StubWorldbuilding = true;
-        public static bool ForceCrayons = false;
 
-        public static List<string> CrayonSeeds = new List<string>();
-        public static SortedSet<int> Rolls = new SortedSet<int>();
-        public static int RollCount = 0;
-
-        public static string Encode(ulong num) {
-            string result = "";
-            do {
-                result = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[(int)(num % 36ul)] + result;
-                num /= 36;
-            } while (num != 0);
-            return result;
-        }
-
-        public static ulong Decode(string num) {
-            ulong result = 0;
-            foreach (var c in num) {
-                result *= 36;
-                if ('0' <= c && c <= '9') {
-                    result += (ulong)(c - '0');
-                } else {
-                    result += (ulong)(c - 'A') + 10;
-                }
-            }
-            return result;
-        }
-
+        public static Random random = new Random();
         public static bool Test() {
             var player = CreateCharacter.Template.PlayerBody;
-            if (player.Inventory.HasObject("BoxOfCrayons")) {
-                CrayonSeeds.Insert(0, Seed);
-            }
             return player.Inventory.HasObject(o => o.GetLongProperty("Nanocrayons") == 1);
         }
 
-        public static bool AfterNewGame() {
-            // only actually run the game if we pass the test
+        public static bool ShouldTryAgain() {
             if (!Test()) {
-                ++_Seed;
-                if (_Seed % 36 == 0) {
-                    using (StreamWriter writer = new StreamWriter("seed.txt")) {
-                        writer.WriteLine(_Seed);
-                    }
-                }
+                Seed = NextSeed();
+                GameManager.Instance.PopGameView(); // clear away the WorldCreationProgress screen
                 return true;
+            }
+            using (StreamWriter writer = new StreamWriter("seeds.txt", true)) {
+                writer.WriteLine(Seed);
             }
             return false;
         }
 
-        public static void Measure() {
-            StateAtMeasure = LastReseed + " + " + RollsSinceReseed;
+        public static string NextSeed() {
+            string result = "";
+            while (result.Length < SeedLength) {
+                result += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[random.Next(36)];
+            }
+            return result;
         }
     }
     [HarmonyPatch(typeof(CreateCharacter), "PickGameType")]
@@ -100,6 +54,17 @@ namespace XRL.SeedFinder {
             XRLCore.Core.Game.PlayerName = State.Name;
             __result = "<manualseed>";
             return false;
+        }
+    }
+    [HarmonyPatch(typeof(Popup), "AskString")]
+    public static class PatchAskString {
+        static bool Prefix(ref string Message, ref string __result) {
+            if (Message == "Enter a world seed.") {
+                State.Seed = State.NextSeed();
+                __result = State.Seed;
+                return false;
+            }
+            return true;
         }
     }
     [HarmonyPatch(typeof(CreateCharacter), "GenerateCharacter")]
@@ -132,26 +97,6 @@ namespace XRL.SeedFinder {
             return false;
         }
     }
-    [HarmonyPatch(typeof(Popup), "AskString")]
-    public static class PatchAskString {
-        static bool Prefix(ref string Message, ref string __result) {
-            if (Message == "Enter a world seed.") {
-                __result = State.Seed;
-                return false;
-            }
-            return true;
-        }
-    }
-    [HarmonyPatch(typeof(Popup), "Show")]
-    public static class PatchShow {
-        static bool Prefix(ref string Message) {
-            if (Message == "You embark for the caves of Qud.") {
-                // no popup for this
-                return false;
-            }
-            return true;
-        }
-    }
     [HarmonyPatch(typeof(XRLCore), "NewGame")]
     public static class PatchNewGame {
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
@@ -171,13 +116,15 @@ namespace XRL.SeedFinder {
                     actual.labels.Add(createCharacterLabel);
                 }
                 yield return actual;
-                if (inst.Is(OpCodes.Call, AccessTools.Method(typeof(CreateCharacter), "GenerateEquipment"))) {
-                    break;
+                if (State.StubWorldbuilding ?
+                        inst.Is(OpCodes.Call, AccessTools.Method(typeof(CreateCharacter), "GenerateEquipment")) :
+                        inst.Is(OpCodes.Call, AccessTools.Method(typeof(GameObject), "UpdateVisibleStatusColor"))) {
+                    yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(State), "ShouldTryAgain"));
+                    // try again instead of returning at the end if ShouldTryAgain tells us to
+                    // if we didn't stub the world building, we need to go all the way to the start to reset the necessary state
+                    yield return new CodeInstruction(OpCodes.Brtrue, State.StubWorldbuilding ? createCharacterLabel : start);
                 }
             }
-            yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(State), "AfterNewGame"));
-            // branch to "start" instead of returning at the end if AfterNewGame tells us to
-            yield return new CodeInstruction(OpCodes.Brtrue, createCharacterLabel);
         }
     }
     [HarmonyPatch(typeof(WorldCreationProgress), "Draw")]
@@ -202,17 +149,11 @@ namespace XRL.SeedFinder {
 
         static void AddToScreenBuffer(ScreenBuffer screenBuffer) {
             var _ = screenBuffer.Goto(5, 22)
-                                .Write("World Seed: " + XRLCore.Core.Game.GetStringGameState("OriginalWorldSeed"))
-                                .Goto(25, 22)
+                                .Write("Seed: " + XRLCore.Core.Game.GetStringGameState("OriginalWorldSeed"))
+                                .Goto(15 + State.SeedLength, 22)
                                 .Write("Memory: " + GC.GetTotalMemory(false))
-                                .Goto(45, 22)
-                                .Write("Best roll: " + State.BestRoll)
-                                .Goto(62, 22)
-                                .Write("Last roll: " + State.LastRoll)
-                                .Goto(25, 21)
-                                .Write("Class: " + CreateCharacter.Template.Genotype + " " + CreateCharacter.Template.Subtype)
-                                .Goto(5, 23)
-                                .Write("Rolls (" + State.RollCount + "): " + State.Rolls.Join());
+                                .Goto(35 + State.SeedLength, 22)
+                                .Write("Class: " + CreateCharacter.Template.Genotype + " " + CreateCharacter.Template.Subtype);
         }
     }
     [HarmonyPatch(typeof(QudHistoryFactory), "GenerateNewSultanHistory")]
@@ -240,65 +181,6 @@ namespace XRL.SeedFinder {
     public static class PatchBuildWorlds {
         static bool Prefix() {
             return !State.StubWorldbuilding;
-        }
-    }
-    [HarmonyPatch(typeof(Crayons), "HandleEvent", new Type[] { typeof(ObjectCreatedEvent) })]
-    public static class PatchHandleEvent {
-        static void Prefix() {
-            State.WantNextRoll = State.InGenerateEquipment;
-        }
-    }
-    [HarmonyPatch(typeof(Stat), "Random", new Type[] { typeof(int), typeof(int) })]
-    public static class PatchRandom {
-        static void Postfix(ref int __result) {
-            if (State.WantNextRoll) {
-                State.LastRoll = __result;
-                State.Rolls.Add(__result);
-                ++State.RollCount;
-                if (__result < State.BestRoll) {
-                    State.BestRoll = __result;
-                }
-                State.WantNextRoll = false;
-            }
-        }
-    }
-    [HarmonyPatch(typeof(Stat), "ReseedFrom", new Type[] { typeof(string), typeof(bool) })]
-    public static class PatchReseedFrom {
-        static void Prefix(ref string Seed) {
-            State.LastReseed = Seed;
-            State.RollsSinceReseed = 0;
-        }
-    }
-    [HarmonyPatch(typeof(Random), "InternalSample")]
-    public static class PatchInternalSample {
-        static void Prefix(Random __instance) {
-            if (__instance == Stat.Rnd) {
-                ++State.RollsSinceReseed;
-            }
-        }
-    }
-    [HarmonyPatch(typeof(CreateCharacter), "GenerateEquipment")]
-    public static class PatchGenerateEquipment {
-        static void Prefix() {
-            State.InGenerateEquipment = true;
-            //State.Measure();
-        }
-        static void Postfix() {
-            State.InGenerateEquipment = false;
-        }
-    }
-    [HarmonyPatch(typeof(World.Encounters.PsychicManager), "Init")]
-    public static class PatchWhereMeasuring {
-        static void Postfix() {
-            State.Measure();
-        }
-    }
-    [HarmonyPatch(typeof(CreateCharacter), "AddItem", new Type[] { typeof(string) })]
-    public static class PatchAddItem {
-        static void Prefix(ref string Blueprint) {
-            if (State.ForceCrayons) {
-                Blueprint = "BoxOfCrayons";
-            }
         }
     }
 }
